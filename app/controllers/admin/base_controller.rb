99eec44475d96fb57c3450a0de5f8ede98c4c536
Yury Cavalcante <yurypcfarias@gmail.com>
2023-03-28 20:01:45 -0300

    feat: add admin permissions in controller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,13 @@
 module Admin
   class BaseController < ApplicationController
     layout 'admin'
+    before_action :authenticate_user!
+    before_action :admin_only!
+
+    private
+
+    def admin_only!
+      redirect_to root_path unless current_user.admin?
+    end
   end
 end
