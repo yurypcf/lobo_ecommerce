75695f0d0704012778daecb0319211762cdeae40
Yury Cavalcante <yurypcfarias@gmail.com>
2023-03-28 19:41:13 -0300

    feat: create admin page and controller

---
+++ app/javascript/admin.js
@@ -0,0 +1,7 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import "./jquery"
+import * as bootstrap from "bootstrap"
+Turbo.session.drive = false
+import "./admin/admin.min.js"
